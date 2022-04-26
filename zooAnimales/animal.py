afb3998c21f56e367498c72e67eab101cc0a9324
JeanCarloMontoya <92815992+JeanCarloMontoya@users.noreply.github.com>
2022-04-25 23:06:54 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -65,9 +65,9 @@ class Animal:
         from zooAnimales.reptil import Reptil
         from zooAnimales.anfibio import Anfibio       
 
-        return ("Mamiferos :"+str(len(Mamifero.getListado()))+"\n"
+        return ("Mamiferos : "+str(len(Mamifero.getListado()))+"\n"
         +"Aves : "+str(len(Ave.getListado()))+"\n"
-        +" Reptiles : "+str(len(Reptil.getListado()))+"\n"
+        +" Reptiles :"+str(len(Reptil.getListado()))+"\n"
         +"Peces : "+str(len(Pez.getListado()))+"\n"
         +"Anfibios : "+str(len(Anfibio.getListado()))+"\n")
 
